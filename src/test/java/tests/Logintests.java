791a5580e38ad52409a2243828940a47ef02390f
Aimila M <almira1612@gmail.com>
2019-02-08 20:37:38 -0500

    adding again

--- src/test/java/tests/Logintests.java
+++ src/test/java/tests/Logintests.java
@@ -8,10 +8,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 public class Logintests {
     WebDriver driver;
+    Map<String,String> myMap = new HashMap<String,String>();
 
     @BeforeMethod
     public void setUp(){
